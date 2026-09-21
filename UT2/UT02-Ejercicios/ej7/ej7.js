bac04414aa4b0292d645774a3d8432ce2379575e
delatorresmr1m <delatorresmr1m@gmail.com>
2026-09-21 23:34:52 +0200

    ej7.js

--- UT2/UT02-Ejercicios/ej7/ej7.js
+++ UT2/UT02-Ejercicios/ej7/ej7.js
@@ -16,7 +16,7 @@ alert(a + b); // 12
 
 let a = prompt("¿Primer número?", 1);
 let b = prompt("¿Segundo número?", 2);
-alert(Number(a) + Number(b)); // 3
+alert(Number(a) + Number(b)); // El resultado ahora es 3
 
 
 
